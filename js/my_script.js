3398bac91bcff9c1eef5acff78dabbfb005979ba
M-E-C-20 <mcasey8511@gmail.com>
2021-01-14 12:10:46 +0000

    added practice test scripts to modify css

---
+++ js/my_script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () { // stops the script from running before the document has loaded
+    $('h2').removeClass('underline'); // can be removed
+    $('ul').removeClass('border'); // can be removed
+})
